6ef8a8e4cf6a0e6bb4261ea71c3bcee03c8df41a
Spencer Benson Njuguna <bennspencer@gmail.com>
2024-08-25 16:42:44 +0300

    configure errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import airQualityRouter from './routes/airQuality.route';
-import './jobs/airQuality.cron';
+import { errorHandler } from './utils/errorHandler';
 import 'dotenv/config';
 
 const app = express();
@@ -9,14 +9,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/air-quality', airQualityRouter);
-
-
+app.use(errorHandler);
 
 const mongoURI = process.env.MONGODB_URI as string;
 const connectionName = process.env.MONGODB_CONNECTION_NAME || 'default';
 
 mongoose.connect(mongoURI, {
-    dbName: connectionName, 
+    dbName: connectionName,
 }).then(() => {
     console.log(`Connected to MongoDB database: ${connectionName}`);
     app.listen(PORT, () => {
@@ -24,6 +23,7 @@ mongoose.connect(mongoURI, {
     });
 }).catch((error) => {
     console.error('MongoDB connection error:', error);
+    process.exit(1);
 });
 
 export default app;
